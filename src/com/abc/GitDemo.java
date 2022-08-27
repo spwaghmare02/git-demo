946b6858bdcc7d15b13441253dbb1eb6dd744121
unknown <spwaghmare.02@gmail.com>
2022-08-27 10:01:31 +0530

    added Data funtion

--- src/com/abc/GitDemo.java
+++ src/com/abc/GitDemo.java
@@ -1,6 +1,10 @@
 package com.abc;
 
 public class GitDemo {
+	public void appData(String data)
+	{
+		System.out.println("data is "+data);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello All");
